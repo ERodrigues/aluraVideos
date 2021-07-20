647c4ea31ca80786d7956c1cb05b103f86934bf6
eder.rodrigues <ederpr@gmail.com>
2021-07-20 20:59:46 -0300

    Adicionado o metodo para retornar os videos pelo seu id

--- src/main/java/com/alura/videos/controller/VideoController.java
+++ src/main/java/com/alura/videos/controller/VideoController.java
@@ -4,6 +4,7 @@ import com.alura.videos.dto.VideoDto;
 import com.alura.videos.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,10 @@ public class VideoController {
     @GetMapping("/videos")
     public List<VideoDto> getVideos(){
         return videoService.getAll();
+    }
+
+    @GetMapping("/videos/{id}")
+    public VideoDto getVideo(@PathVariable long id){
+        return videoService.getById(id);
     }
 }
