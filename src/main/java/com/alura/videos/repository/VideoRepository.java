00ca4742ab0ca20ce07abe7d01459fdb7ed55345
eder.rodrigues <ederpr@gmail.com>
2021-07-29 23:14:11 -0300

    Adicao do metodo que faz a consulta no banco que retorna os videos de acordo com a categoria

--- src/main/java/com/alura/videos/repository/VideoRepository.java
+++ src/main/java/com/alura/videos/repository/VideoRepository.java
@@ -2,8 +2,17 @@ package com.alura.videos.repository;
 
 import com.alura.videos.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    @Query(value = "select b.id, b.titulo, b.descricao, b.url, b.id_categoria " +
+                   "from videos.biblioteca b, videos.categoria c " +
+                   "where b.id_categoria = c.id " +
+                   "and c.id = :categoriaId", nativeQuery = true)
+    public List<Video> getVideosByCategoria(@Param("categoriaId") Long categoriaId);
 }
