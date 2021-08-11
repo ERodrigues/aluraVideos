3d2827566a797284e4b373f59c08e13138fbd861
eder.rodrigues <ederpr@gmail.com>
2021-08-10 23:02:28 -0300

    Implementacao da anotacao para limpar o cache do projeto

--- src/main/java/com/alura/videos/controller/VideoController.java
+++ src/main/java/com/alura/videos/controller/VideoController.java
@@ -5,6 +5,7 @@ import com.alura.videos.service.VideoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -37,18 +38,21 @@ public class VideoController {
 
     @ApiOperation(value="Cadastra um novo vídeo na base de dados")
     @PostMapping
+    @CacheEvict("VideoByCategoria")
     public ResponseEntity<VideoDto> saveVideo(@RequestBody @Valid VideoDto videoDto){
         return new ResponseEntity<>(videoService.save(videoDto), HttpStatus.OK);
     }
 
     @ApiOperation(value="Altera um video de acordo com o ID informado")
     @PutMapping("/alterar/{id}")
+    @CacheEvict("VideoByCategoria")
     public ResponseEntity<VideoDto> updateVideo(@PathVariable long id, @RequestBody @Valid VideoDto videoDto){
         return new ResponseEntity<>(videoService.update(id, videoDto), HttpStatus.OK);
     }
 
     @ApiOperation(value="Deleta um video de acordo com o seu ID")
     @DeleteMapping("/{id}")
+    @CacheEvict("VideoByCategoria")
     public ResponseEntity<String> deleteVideo(@PathVariable Long id){
         var isRemoved = videoService.delete(id);
         if (!isRemoved){
