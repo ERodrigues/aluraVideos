c64cf5f067aa7072e31f2cf78605976aab844a55
eder.rodrigues <ederpr@gmail.com>
2021-08-11 22:07:31 -0300

    Adicao da classe que fara a configuracao do módulo de seguranca da aplicação

--- src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
+++ src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
@@ -1,4 +1,19 @@
 package com.alura.videos.config.security;
 
-public class SecurityConfigurations {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /* Parametriza quais URLs do projeto serão publicas */
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/categorias").permitAll()
+                .anyRequest().authenticated();
+    }
 }
