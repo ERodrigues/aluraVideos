892321b8068e7742bdada8f29ff5170fa3b74540
eder.rodrigues <ederpr@gmail.com>
2021-08-12 22:12:27 -0300

    Implementacao dos metodos de autenticacao

--- src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
+++ src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
@@ -1,14 +1,21 @@
 package com.alura.videos.config.security;
 
+import com.alura.videos.config.security.service.AutenticacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         /* Parametriza quais URLs do projeto serão publicas */
@@ -16,5 +23,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/categorias").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
