ccfe50818bd621b98b0013ad72001c57b36969a0
eder.rodrigues <ederpr@gmail.com>
2021-08-16 22:37:19 -0300

    Mudanca na configuracao da classe de seguranca para implementacao de autenticacao sem sessionID

--- src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
+++ src/main/java/com/alura/videos/config/security/SecurityConfigurations.java
@@ -2,12 +2,15 @@ package com.alura.videos.config.security;
 
 import com.alura.videos.config.security.service.AutenticacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -17,12 +20,20 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     private AutenticacaoService autenticacaoService;
 
     @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         /* Parametriza quais URLs do projeto serão publicas */
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/categorias").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
