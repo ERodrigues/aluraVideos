ac59adf3c8dfdba04c40a773a63b89497ff0490f
eder.rodrigues <ederpr@gmail.com>
2021-08-16 22:35:15 -0300

    Classe para tratar as informações de login

--- src/main/java/com/alura/videos/dto/LoginDto.java
+++ src/main/java/com/alura/videos/dto/LoginDto.java
@@ -1,2 +1,29 @@
-package com.alura.videos.dto;public class LoginDto {
+package com.alura.videos.dto;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class LoginDto {
+
+    public String login;
+    public String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public UsernamePasswordAuthenticationToken convert() {
+        return new UsernamePasswordAuthenticationToken(login, senha);
+    }
 }
