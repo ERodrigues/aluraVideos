d15ac301224223f01dc1246af6d745767acd8b1e
Eder Rodrigues <ederpr@gmail.com>
2021-08-17 22:00:26 -0300

    Classe que vai retornar o token e o tipo de autenticacao

---
+++ src/main/java/com/alura/videos/dto/TokenDto.java
@@ -0,0 +1,27 @@
+package com.alura.videos.dto;
+
+public class TokenDto {
+    private String token;
+    private String bearer;
+
+    public TokenDto(String token, String bearer) {
+        this.token = token;
+        this.bearer = bearer;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+
+    public void setBearer(String bearer) {
+        this.bearer = bearer;
+    }
+}
