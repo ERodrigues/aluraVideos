bf303281d4ea5dddf5097ff745c849a6b460c7ad
Eder Rodrigues <ederpr@gmail.com>
2021-08-23 16:07:53 -0300

    Adicionado o metodo a interface do repositorio do usuario para retorno do usuario pelo id

--- src/main/java/com/alura/videos/repository/UsuarioRepository.java
+++ src/main/java/com/alura/videos/repository/UsuarioRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findById(Long id);
 }
