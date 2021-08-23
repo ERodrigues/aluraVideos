f9652e995f7c0f294b0639d0f75360efef6d4014
Eder Rodrigues <ederpr@gmail.com>
2021-08-23 16:04:32 -0300

    Criado o método de construtor do filtro para receber a classe de serviço e repositório como parâmtro; Implementado o método de validação de autenticação do usuario via token; Correção do método que retorna o token da API, retirando o tipo do token do retorno

--- src/main/java/com/alura/videos/config/security/AutenticaViaTokenFilter.java
+++ src/main/java/com/alura/videos/config/security/AutenticaViaTokenFilter.java
@@ -1,5 +1,10 @@
 package com.alura.videos.config.security;
 
+import com.alura.videos.config.security.service.TokenApiService;
+import com.alura.videos.model.Usuario;
+import com.alura.videos.repository.UsuarioRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,22 +12,42 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 public class AutenticaViaTokenFilter extends OncePerRequestFilter {
 
+    private TokenApiService tokenService;
+    private UsuarioRepository userRepository;
+
+    public AutenticaViaTokenFilter(TokenApiService tokenService, UsuarioRepository userRepository) {
+        this.tokenService = tokenService; 
+        this.userRepository = userRepository;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String token = recuperaToken(request);
+        String token = recoverToken(request);
+
+        if (tokenService.isValid(token)){
+            authenticateUser(token);
+        }
         filterChain.doFilter(request, response);
     }
 
-    private String recuperaToken(HttpServletRequest request) {
+    private void authenticateUser(String token) {
+        Long idUser = tokenService.getIdUser(token);
+        Usuario user = userRepository.findById(idUser).get();
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
 
         if (token == null || !token.startsWith("Bearer ")){
             return null;
         }
 
-        return token;
+        return token.substring(7, token.length());
     }
 }
