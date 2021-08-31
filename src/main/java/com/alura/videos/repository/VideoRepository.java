87324851858f194be295488675eb2318a08e2008
Eder Rodrigues <ederpr@gmail.com>
2021-08-31 14:35:17 -0300

    Remoção da referência query Param que não está sendo mais utilizada no método findByCategoriaId

--- src/main/java/com/alura/videos/repository/VideoRepository.java
+++ src/main/java/com/alura/videos/repository/VideoRepository.java
@@ -4,13 +4,10 @@ import com.alura.videos.model.Video;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
-    public Page<Video> findByCategoriaId(@Param("categoriaId") Long categoriaId, Pageable pages);
-    public Page<Video> findByTituloContainingIgnoreCase (String search, Pageable pages);
+    Page<Video> findByCategoriaId(Long categoriaId, Pageable pages);
+    Page<Video> findByTituloContainingIgnoreCase (String search, Pageable pages);
 }
